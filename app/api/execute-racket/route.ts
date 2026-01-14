17157399b56f81f8a149e3ccb7dd13aabf1a8567
hiroto0501 <ee227031@meiji.ac.jp>
2026-01-15 02:03:31 +0900

    1/15 vercel用にコードを修正

--- app/api/execute-racket/route.ts
+++ app/api/execute-racket/route.ts
@@ -4,6 +4,8 @@ import { promisify } from 'util';
 import fs from 'fs/promises';
 import path from 'path';
 
+export const dynamic = 'force-dynamic';
+
 const execAsync = promisify(exec);
 
 export async function GET() {
